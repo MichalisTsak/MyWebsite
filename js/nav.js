6ade1178d2eb3127fc3d79223c75b9071145c67b
MichalisTsak <tsakirmich@gmail.com>
2024-02-13 22:34:12 +0200

    a lot have changed, i have added a navbar, a timeline, color, different fonts, style, more css

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+// nav.js
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  const mainbar = document.querySelector('.nav-list')
+  sidebar.style.display = 'flex'
+  mainbar.style.display = 'none' 
+}
+  
